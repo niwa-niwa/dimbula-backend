0e727b83b1a7cf798d38fbe8e69a0fc25402ba5e
niwa-niwa <urban.hang274@gmail.com>
2021-04-18 16:26:50 +0900

    implemented CORS

--- config/settings.py
+++ config/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
 
     'rest_framework',
+    'corsheaders',
     'firebase_auth',
 
     'v1.apps.V1Config',
@@ -47,6 +48,7 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
@@ -158,3 +160,8 @@ FIREBASE_AUTH = {
         os.getenv('FIREBASE_AUTH_EMAIL_VERIFICATION', True),
 }
 # ====>
+
+CORS_ALLOW_CREDENTIALS = True
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:3000",
+]
