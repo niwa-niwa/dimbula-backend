81c9c8082b59ab780fc8ede5cf9483f9e0894ff5
niwa-niwa <urban.hang274@gmail.com>
2021-08-31 21:59:53 +0900

    changed DB settings to use SQLite

--- config/settings.py
+++ config/settings.py
@@ -94,18 +94,18 @@ WSGI_APPLICATION = 'config.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 DATABASES = {
-    # 'default': {
-    #     'ENGINE': 'django.db.backends.sqlite3',
-    #     'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    # }
     'default': {
-        'ENGINE':'django.db.backends.postgresql_psycopg2',
-        'NAME':'dimbula',
-        'USER':os.environ.get('DB_USER'),
-        'PASSWORD':os.environ.get('DB_PASSWORD'),
-        'HOST':'',
-        'PORT':'',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
+    # 'default': {
+    #     'ENGINE':'django.db.backends.postgresql_psycopg2',
+    #     'NAME':'dimbula',
+    #     'USER':os.environ.get('DB_USER'),
+    #     'PASSWORD':os.environ.get('DB_PASSWORD'),
+    #     'HOST':'',
+    #     'PORT':'',
+    # }
 }
 
 # Password validation
